173abf680d5e871b0908c4465e3ccb0b89eaaa15
Scottie 2823 <teamautomatons2823@gmail.com>
2019-02-11 16:04:23 -0600

    Lightvalues python changes

--- lightvalues.py
+++ lightvalues.py
@@ -3,12 +3,10 @@ import time
 from networktables import NetworkTables
 import threading
 
-
-value = 0
 # buttons[i][0] = name, buttons[i][1] = default value
-buttons = [["Reset Button", False], ["Hatch Level 3", False], ["Hatch Level 2", False], ["Hatch Level 1", False],
-         ["Cargo Level 1", False], ["Cargo Level 2", False], ["Cargo Level 3", False], ["Cargo Level 1", False],
-         ["Hatch Level 1", False], ["Magic Button", False], ["SDS In", False],
+buttons = [["Reset Button", False], ["Hatch Level 3", False], ["Hatch Level 1", False], ["Hatch Level 2", False],
+         ["Cargo Level 1", False], ["Cargo Level 2", False], ["Cargo Level 3", False], ["Cargo Ship Cargo", False],
+         ["Cargo Ship Hatch", False], ["Magic Button", False], ["SDS In", False],
          ["SDS Out", False], ["Hatch Out", False], ["Hatch In", False], ["nil", False], ["nil", False]]
 
 #NetworkTables
@@ -21,7 +19,7 @@ def connectionListener(connected, info):
         notified[0] = True
         cond.notify()
 
-NetworkTables.initialize(server='10.28.23.2')
+NetworkTables.initialize(server='roboRIO-2823-FRC.local')
 NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 
 with cond:
@@ -33,15 +31,12 @@ print("Connected!")
 
 table = NetworkTables.getTable('SmartDashboard')
 while True:
+    value=0
     for i, v in enumerate(buttons):
-        v[1] = table.getBoolean(v[0], v[1])
-        if v[1] == True:
+        if table.getBoolean(v[0], v[1]) == True:
             value |= (1 << i)
     
     flag = '{:04X}'.format(value)
-    subprocess.Popen(["lightvalues.exe", flag], shell=True)
-    print(flag)
-    time.sleep(0.1)
-
+    subprocess.Popen(["lightvalues.exe", flag], shell=True).wait()
 
 
